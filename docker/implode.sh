6b079ae8bcce79f781e39a7402935b863fea52eb
Marco Antonio Gonzalez Junior <marcoantoniogonzalezjunior@gmail.com>
2018-04-23 15:13:55 -0300

    Delete ALL local Docker containers and images.

---
+++ docker/implode.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Remove all Docker images and containers
+# WARNING: Cannot be undone
+
+function rm_all {
+	docker stop $(docker ps -a -q)
+	docker rm $(docker ps -a -q)
+	docker rmi $(docker images -q) --force
+}
+
+function do_nothing() {
+	echo -e '\033[1;32mOK! Bye\033[m'
+	exit 0
+}
+
+read -r -p "Are you sure? [yes/N]: " response
+case "$response" in
+    [yY][eE][sS]|[yY])
+        rm_all
+        ;;
+    *)
+        do_nothing
+        ;;
+esac
